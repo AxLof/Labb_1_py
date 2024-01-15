8eb8d9feaefbe5b6198bb0eb608690b8ea0e0373
Axel Löfgren <h23axlof@du.se>
2024-01-15 16:49:29 +0100

    First commit

---
+++ modules.py
@@ -0,0 +1,39 @@
+import random
+
+def divisible_integers(firstint, secondint):
+
+    division_list=[]
+
+    for i in range(1, 1601):
+
+        if i % firstint == 0 and i % secondint == 0:
+            division_list.append(i)
+
+    return division_list
+
+def guessing_numbers():
+
+    random_number = random.randint(1, 60)
+    print('\nGuess a number between 1 and 60')
+    input_number = int
+    while input_number != random_number:
+
+        try:
+            input_number = int(input())
+
+            if input_number == random_number:
+                print(f'Correct, the number is {random_number}!')
+
+            elif input_number > random_number:
+                print('Too high, try again!')
+
+            elif input_number < random_number:
+                print('Too low, try again!')
+        except ValueError:
+            print('Wrong input type!')
+            print('\nGuess a number between 1 and 60')
+    return
+
+
+
+
---
+++ program.py
@@ -0,0 +1,36 @@
+import modules
+
+
+user_choice = int
+
+while user_choice != 3:
+    try:
+        print('\nMake a choice by entering a number between 1 and 3\n'
+              '[1]: Divide two numbers\n'
+              '[2]: Guess the number\n'
+              '[3]: Exit program')
+        user_choice = int(input())
+
+        match user_choice:
+            case 1:
+                firstint = int(input('\nEnter first number: '))
+                secondint = int(input('\nEnter second number: '))
+                print('\n', modules.divisible_integers(firstint, secondint))
+                continue
+
+            case 2:
+                modules.guessing_numbers()
+                continue
+
+            case 3:
+                exit()
+
+            case default:
+                print('\nPlease enter a valid number!')
+    except ValueError:
+        print('\nWrong input, please enter a number!')
+
+
+
+
+
